9c362839c09ba862ecfc69be3e9044c4e57ba530
Vawser <ben.rouncefield@gmail.com>
2018-07-28 00:09:49 +0100

    Seven Kingdoms
    Changed the Seven Kingdoms interface

--- common/defines/AI.lua
+++ common/defines/AI.lua
@@ -1,8 +1,8 @@
 --------------------------------
 -- AI
 --------------------------------
-NDefines.NAI.AGGRESSIVENESS = 2000 -- Base chance (out of 10000) of AI being willing to start a war each diplomatic tick (~1.5 times a month)
-NDefines.NAI.AGGRESSIVENESS_BONUS_EASY_WAR = 1500 -- Added to aggressiveness if the war is against a weak or particularily hated enemy
+NDefines.NAI.AGGRESSIVENESS = 2000
+NDefines.NAI.AGGRESSIVENESS_BONUS_EASY_WAR = 1500
 NDefines.NAI.AI_USES_HISTORICAL_IDEA_GROUPS = 0  
 
 

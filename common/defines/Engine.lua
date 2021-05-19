6b8bb113c4418e3343ba090c42a6c597545ee831
Vawser <ben.rouncefield@gmail.com>
2021-05-20 00:11:44 +0100

    Update
    - Updated for 1.31

--- common/defines/Engine.lua
+++
@@ -1,8 +0,0 @@
---------------------------------
--- Game
---------------------------------
-NDefines.NGame.START_DATE = "100.1.1"
-NDefines.NGame.END_DATE = "1000.1.2"
-NDefines.NGame.MAX_RANDOM_NEW_WORLD = "1000.1.2"
-NDefines.NEngine.EVENT_PROCESS_OFFSET = 30
-
